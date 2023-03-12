b192c84d20d0904cb114b489c49b3a5141f1d373
giusepper11 <giusepper11@gmail.com>
2023-03-11 21:55:02 -0300

    WIP:CH03

--- ch03/data_types/src/main.rs
+++ ch03/data_types/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let guess: u32 = "42".parse().expect("Not a number");
+    println!("guess={guess}");
+
+    // Scalar types
+    // integer
+    // i32 - integer can be negative or positve
+    // u32 - integer can only be positive
 }
